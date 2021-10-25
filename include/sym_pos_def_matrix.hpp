18a1ba33634bc9ae56619140b1f1fec419c7ed77
Anjan Roy <anjanroy@yandex.com>
2021-10-25 08:13:10 +0000

    adding two square matrices, extracted out

--- include/sym_pos_def_matrix.hpp
+++ include/sym_pos_def_matrix.hpp
@@ -2,3 +2,6 @@
 
 void transpose(sycl::queue &q, float *const mat, const uint dim,
                const uint wg_size);
+
+void add(sycl::queue &q, const float *mat_a, const float *mat_b,
+         float *const mat_c, const uint dim, const uint wg_size);
--- sym_pos_def_matrix.cpp
+++ sym_pos_def_matrix.cpp
@@ -22,3 +22,32 @@ void transpose(sycl::queue &q, float *const mat, const uint dim,
   });
   evt.wait();
 }
+
+void add(sycl::queue &q, const float *mat_a, const float *mat_b,
+         float *const mat_c, const uint dim, const uint wg_size) {
+  sycl::buffer<float, 2> b_mat_a{mat_a, sycl::range<2>{dim, dim}};
+  sycl::buffer<float, 2> b_mat_b{mat_b, sycl::range<2>{dim, dim}};
+  sycl::buffer<float, 2> b_mat_c{mat_c, sycl::range<2>{dim, dim}};
+
+  auto evt = q.submit([&](sycl::handler &h) {
+    sycl::accessor<float, 2, sycl::access::mode::read,
+                   sycl::access::target::global_buffer>
+        a_mat_a{b_mat_a, h};
+    sycl::accessor<float, 2, sycl::access::mode::read,
+                   sycl::access::target::global_buffer>
+        a_mat_b{b_mat_b, h};
+    sycl::accessor<float, 2, sycl::access::mode::write,
+                   sycl::access::target::global_buffer>
+        a_mat_c{b_mat_c, h, sycl::noinit};
+
+    h.parallel_for<class kernelAdd>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const uint r = it.get_global_id(0);
+          const uint c = it.get_global_id(1);
+
+          a_mat_c[r][c] = a_mat_a[r][c] + a_mat_b[r][c];
+        });
+  });
+  evt.wait();
+}
