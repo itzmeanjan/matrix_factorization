cab21fb6ea74ac60bd04e75efc7e2511aefbeccb
Anjan Roy <anjanroy@yandex.com>
2021-10-25 08:48:11 +0000

    random matrix generation routine; display matrix routine

---
+++ include/utils.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <random>
+
+void random_matrix(float *const matrix, const uint dim);
+
+void show(const float *mat, const uint dim);
---
+++ utils.cpp
@@ -0,0 +1,23 @@
+#include "utils.hpp"
+
+void random_matrix(float *const matrix, const uint dim) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<float> dis(0.f, 1.f);
+
+  for (uint i = 0; i < dim; i++) {
+    for (uint j = 0; j < dim; j++) {
+      matrix[i * dim + j] = dis(gen);
+    }
+  }
+}
+
+void show(const float *mat, const uint dim) {
+  for (uint i = 0; i < dim; i++) {
+    for (uint j = 0; j < dim; j++) {
+      std::cout << mat[i * dim + j] << "\t";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
